1f6f1bd3c8080a124858b859b359d0bcd1c85e10
sergiga <srgarcia91@gmail.com>
2019-09-21 13:15:57 +0200

    Created workout models

--- workouts/models.py
+++ workouts/models.py
@@ -5,4 +5,43 @@ class Workout(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return '{} - {}'.format(self.id, self.name)
+        return self.name
+
+
+class Round(models.Model):
+    name = models.CharField(max_length=100)
+    order = models.IntegerField()
+
+    workout = models.ForeignKey(Workout, null=True, related_name='rounds', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return '{}. Order: {}'.format(self.name, self.order)
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    rounds = models.ManyToManyField(Round, related_name='exercises', through='RoundSet')
+
+    def __str__(self):
+        return self.name
+
+
+class RoundSet(models.Model):
+    order = models.IntegerField()
+    repetitions = models.IntegerField(null=True)
+    seconds = models.IntegerField(null=True)
+
+    workout_round = models.ForeignKey(Round, related_name='sets', on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, related_name='sets', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}. Order: {}'.format(self.workout_round.name, self.exercise.name, self.order)
+
+
+class Muscle(models.Model):
+    name = models.CharField(max_length=100)
+
+    exercises = models.ManyToManyField(Exercise, related_name='muscles')
+
+    def __str__(self):
+        return self.name

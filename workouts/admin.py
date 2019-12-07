3e1d0e81f8dc7145b88045a6fdfa0120be9744f9
sergiga <srgarcia91@gmail.com>
2019-12-07 13:32:15 +0100

    Start exercises app

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from workouts.models import (
-    Workout, Round, RoundSet, Exercise, Muscle
+    Workout, WorkoutSet, ExerciseInSet,
+    Training, TrainingExercise
 )
 
-@admin.register(Workout, Round, RoundSet, Exercise, Muscle)
+@admin.register(
+    Workout, WorkoutSet, ExerciseInSet,
+    Training, TrainingExercise
+)
 class AuthorAdmin(admin.ModelAdmin):
     pass

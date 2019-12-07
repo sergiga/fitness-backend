42bc5346252341bee97c20ef269f6412ffb1bb2c
sergiga <srgarcia91@gmail.com>
2019-12-07 13:37:57 +0100

    Exercise app models

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from core.models import BaseModel
+from core.utils import Level
 
-# Create your models here.
+
+class Exercise(BaseModel):
+    name = models.CharField(max_length=100)
+    level = models.IntegerField(
+        choices=Level.choices(),
+        default=Level.INTERMEDIATE
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Muscle(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Equipment(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MuscleInExercise(BaseModel):
+    exercise = models.ForeignKey(
+        'exercises.Exercise',
+        related_name='muscles_in_exercise',
+        on_delete=models.CASCADE
+    )
+    muscle = models.ForeignKey(
+        'exercises.Muscle',
+        related_name='muscles_in_exercise',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return '{} - {}'.format(
+            self.exercise.__str__(),
+            self.muscle.__str__()
+        )
+
+
+class EquipmentInExercise(BaseModel):
+    exercise = models.ForeignKey(
+        'exercises.Exercise',
+        related_name='muscles_in_exercise',
+        on_delete=models.CASCADE
+    )
+    equipment = models.ForeignKey(
+        'exercises.Equipment',
+        related_name='equipments_in_exercise',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return '{} - {}'.format(
+            self.exercise.__str__(),
+            self.equipment.__str__()
+        )
